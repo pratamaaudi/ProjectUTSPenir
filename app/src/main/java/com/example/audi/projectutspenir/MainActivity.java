00c7375b8105243b95e9b3d693903a38631556de
pratamaaudi <pratamaaudi@gmail.com>
2017-04-02 10:33:48 +0700

    CODE
    - button arcade dan button catch on click

--- app/src/main/java/com/example/audi/projectutspenir/MainActivity.java
+++ app/src/main/java/com/example/audi/projectutspenir/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.audi.projectutspenir;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnarcade, btncatch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnarcade = (Button) findViewById(R.id.btnarcade);
+        btnarcade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buattoast("button arcade");
+            }
+        });
+
+        btncatch = (Button) findViewById(R.id.btncatch);
+        btncatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buattoast("button catch em all");
+            }
+        });
+    }
+
+    public void buattoast(String pesan) {
+        Toast.makeText(MainActivity.this, pesan, Toast.LENGTH_SHORT).show();
     }
 }
