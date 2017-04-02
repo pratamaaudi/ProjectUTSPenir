a0813e7c3e56993465b2bcb1eee0abaa89cc47c1
pratamaaudi <pratamaaudi@gmail.com>
2017-04-02 12:04:36 +0700

    CODE
    - hilangkan header
    - undo button
    - set skor
    - set kesempatan

--- app/src/main/java/com/example/audi/projectutspenir/arcade.java
+++ app/src/main/java/com/example/audi/projectutspenir/arcade.java
@@ -4,19 +4,44 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class arcade extends AppCompatActivity {
 
-    ArrayList<Integer> historybutton = new ArrayList<Integer>();
+    ArrayList<Integer> historybutton = new ArrayList<>();
+    TextView txtkesempatan, txtskor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hilangkanheader();
         setContentView(R.layout.activity_arcade);
+
+        setskor(0);
+
+        setkesempatan(3);
+    }
+
+    public void hilangkanheader() {
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
+    }
+
+    public void setskor(int skor) {
+        txtskor = (TextView) findViewById(R.id.txtskor);
+        txtskor.setText("Skor : " + String.valueOf(skor));
+    }
+
+    public void setkesempatan(int kesempatan) {
+        txtkesempatan = (TextView) findViewById(R.id.txtkesempatan);
+        txtkesempatan.setText("Kesempatan : " + String.valueOf(kesempatan));
     }
 
     public void buttonjwbklik(View view) {
@@ -25,18 +50,28 @@ public class arcade extends AppCompatActivity {
         historybutton.add(view.getId());
         view.setVisibility(View.INVISIBLE);
 
-        buattoast("button "+view.getId()+" masuk ke array dan invisibel");
+        buattoast("button " + view.getId() + " masuk ke array dan invisibel");
     }
 
     public void buttonisiklik(View view) {
         buattoast("button jawaban");
 
-        int itemterakhir = historybutton.size()-1;
+        int itemterakhir = historybutton.size() - 1;
+        if (itemterakhir >= 0) {
+            restorebutton(itemterakhir);
+        }
+    }
+
+    public void restorebutton(int itemterakhir) {
         Button restore = (Button) findViewById(historybutton.get(itemterakhir));
 
         restore.setVisibility(View.VISIBLE);
 
         historybutton.remove(itemterakhir);
+    }
+
+    public void imageklik(View view) {
+        buattoast("salah sasaran bosku -_-");
     }
 
     public void buattoast(String pesan) {
