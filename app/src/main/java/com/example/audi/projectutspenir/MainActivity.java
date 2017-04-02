96eecc38ab71e284fffd5997787106b5fa22836c
pratamaaudi <pratamaaudi@gmail.com>
2017-04-02 10:52:43 +0700

    CODE
    - pindah ke arcade mode saat button di klik

--- app/src/main/java/com/example/audi/projectutspenir/MainActivity.java
+++ app/src/main/java/com/example/audi/projectutspenir/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.audi.projectutspenir;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 buattoast("button arcade");
+                Intent i = new Intent(getApplicationContext(), arcade.class);
+                startActivity(i);
             }
         });
 
